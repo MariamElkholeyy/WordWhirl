742775f6023c7dd0909b5c5e20a921beee7b39ad
Mariam Elkholey <mariamelkholey.grand@gmail.com>
2024-10-29 04:52:59 +0300

    first commit

---
+++ WordWhirl.py
@@ -0,0 +1,13 @@
+# This Takes a word as input and calculates the number of possible permutations.
+
+import itertools
+
+def word_shuffler(word):
+    permutations = itertools.permutations(word)
+    num_permutations = len(list(permutations))
+    return num_permutations
+
+if __name__ == "__main__":
+    word = input("Enter a word: ")
+    num_permutations = word_shuffler(word)
+    print(f"The word '{word}' can be shuffled in {num_permutations} different ways.")
